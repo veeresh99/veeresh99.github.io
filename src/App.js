1501136b64d25579ff69cfab4d0116cbd701a82c
veeresh99 <veeresh2@illinois.edu>
2022-01-17 10:00:28 -0800

    new changes

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
         <p className="welcome_text">welcome! i'm <a href="https://www.linkedin.com/in/veeresh-neralagi/" target="_blank">veeresh</a> and here's what i've been up to.</p>
       </div>
       <Container className="sub-container">
-        <Row>
-          <div className="title-div">
+        <div className="title-div">
             <h2>Projects</h2>
-          </div>
+        </div>
+        <Row>
           <div className="card card-1">
             <p className="title"><a href="https://shoestack.vercel.app/" target="_blank" className="a-div">Shoestack</a></p>
             <p className="description">Building a platform that helps runners efficiently find their next pair of running shoes.</p>
@@ -22,10 +22,10 @@ function App() {
             <p className="description">Creating a 3 part lecture series on an introduction to website development! </p>
           </div>
         </Row>
-        <Row>
-          <div className="title-div">
+        <div className="title-div">
             <h2>Writing</h2>
-          </div>
+        </div>
+        <Row>
           <div className="card card-3">
             <p className="title"><a href="https://the-transcendence.vercel.app/" target="_blank" className="a-div">The Transcendence</a></p>
             <p className="description"> Bringing the most authentic stories in the world of endurance sports and redefining what's possible. </p>
@@ -35,10 +35,10 @@ function App() {
             <p className="description"> I write about technology and interview notable people in various disciplines. </p>
           </div>
         </Row>
+        <div className="title-div">
+          <h2>Running</h2>
+        </div>
         <Row>
-          <div className="title-div">
-            <h2>Running</h2>
-          </div>
           <div className="card card-6">
             <p className="title"><a href="https://www.baa.org/" target="_blank" className="a-div">Boston Marathon</a></p>
             <p className="description"> Excited to be training for my first Marathon major on April 18th in Boston!   </p>
